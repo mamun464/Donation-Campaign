44fc0724669f7693d80502da98c1de661caf77ef
Mamun <mamunurrashid.s.bd@gmail.com>
2024-01-19 13:07:02 +0600

    does not exits id handle

--- src/Components/DonationPage/DonationPage.jsx
+++ src/Components/DonationPage/DonationPage.jsx
@@ -6,6 +6,7 @@ import 'react-toastify/dist/ReactToastify.css';
 const DonationPage = () => {
     const { id } = useParams();
     const [donations, setDonation] = useState([]);
+    const [Displayed, setDisplayed] = useState(true);
 
     useEffect(() => {
         fetch('/donations.json')
@@ -36,31 +37,45 @@ const DonationPage = () => {
 
     };
 
+    useEffect(() => {
+        if (donationById) {
+            setDisplayed(true);
+        } else {
+            setDisplayed(false);
+        }
+
+    }, [donationById]);
+
     return (
         <div className="px-8 max-w-7xl mx-auto mt-32 relative">
-            <div className="flex justify-center">
-                <div className="relative">
-                    {/* Image */}
-                    <img className="mt-6 xs:h-[350px] xs:w-[750px] md:h-[700px] md:w-[1300px] md:mt-0 rounded-lg" src={donationById?.image} alt="Donation_No_Data_Found" />
 
-                    {/* Overlay with Donate button at the bottom */}
-                    <div className="h-20 flex items-center justify-center xs:justify-start absolute left-0 bottom-0 bg-black bg-opacity-50 p-4 w-full xs:h-32 rounded-lg rounded-tl-none rounded-tr-none">
-                        <button
-                            className="btn py-2 px-3 text-white font-semibold text-xl  xs:ml-8 rounded-[4px] border-0"
-                            style={buttonStyle}
-                            onClick={() => donationOperation(donationById?.id)}
-                        >
-                            Donate ${donationById?.price}
-                        </button>
+            {Displayed && (
+                <div>
+                    <div className="flex justify-center">
+                        <div className="relative">
+                            {/* Image */}
+                            <img className="mt-6 xs:h-[350px] xs:w-[750px] md:h-[700px] md:w-[1300px] md:mt-0 rounded-lg" src={donationById?.image} alt="Donation_No_Data_Found" />
+
+                            {/* Overlay with Donate button at the bottom */}
+                            <div className="h-20 flex items-center justify-center xs:justify-start absolute left-0 bottom-0 bg-black bg-opacity-50 p-4 w-full xs:h-32 rounded-lg rounded-tl-none rounded-tr-none">
+                                <button
+                                    className="btn py-2 px-3 text-white font-semibold text-xl  xs:ml-8 rounded-[4px] border-0"
+                                    style={buttonStyle}
+                                    onClick={() => donationOperation(donationById?.id)}
+                                >
+                                    Donate ${donationById?.price}
+                                </button>
+
+                            </div>
+                        </div>
 
                     </div>
+                    <div className="mt-12 mb-12">
+                        <h1 className="text-center md:text-start text-[#0B0B0B] font-bold text-5xl">{donationById?.title}</h1>
+                        <p className="mt-6 text[#0B0B0BB3] text-justify leading-7">{donationById?.description}</p>
+                    </div>
                 </div>
-
-            </div>
-            <div className="mt-12 mb-12">
-                <h1 className="text-center md:text-start text-[#0B0B0B] font-bold text-5xl">{donationById?.title}</h1>
-                <p className="mt-6 text[#0B0B0BB3] text-justify leading-7">{donationById?.description}</p>
-            </div>
+            )}
 
             <ToastContainer />
         </div>
